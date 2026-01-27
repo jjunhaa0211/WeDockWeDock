b0a75c9d93ee8a2021ee34e7bcbc3bbf9388f2fb
jjunhaa0211 <goodjunha@gmail.com>
2026-01-27 14:30:58 +0900

    Initial Commit

---
+++ WeDockWeDock/WeDockWeDockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeDockWeDockApp.swift
+//  WeDockWeDock
+//
+//  Created by 박준하 on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct WeDockWeDockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeDockWeDockTests/WeDockWeDockTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeDockWeDockTests.swift
+//  WeDockWeDockTests
+//
+//  Created by 박준하 on 1/27/26.
+//
+
+import Testing
+@testable import WeDockWeDock
+
+struct WeDockWeDockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
